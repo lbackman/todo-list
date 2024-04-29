dc329ca659c93e92036f38de27f485314d65af28
lbackman <lucasbackman@gmail.com>
2024-04-29 17:33:22 +0300

    Create modals for projects and todos

--- src/index.js
+++ src/index.js
@@ -22,3 +22,24 @@ defaultProject.addTodo(td)
 console.log(defaultProject.projectTodos)
 defaultProject.removeTodo(td)
 console.log(defaultProject.projectTodos)
+
+const projectModalBtn = document.getElementById("new-project")
+const todoModalBtn = document.getElementById("new-todo")
+
+const closeModal = function(modal) {
+  modal.style.display = "none"
+}
+
+const buttons = [projectModalBtn, todoModalBtn]
+buttons.forEach(button => {
+  button.onclick = function() {
+    const modal = button.nextElementSibling
+    modal.style.display = 'block'
+    const closeSpan = modal.querySelector('.close')
+    window.onclick = function(event) {
+      if (event.target == modal || event.target == closeSpan) {
+        closeModal(modal)
+      }
+    }
+  }
+})
