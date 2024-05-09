33209d256bf20d1de1cc5a18b32ac76c7c65cf65
lbackman <lucasbackman@gmail.com>
2024-05-09 11:57:44 +0300

    Refactor modal button event listener

--- src/index.js
+++ src/index.js
@@ -19,6 +19,13 @@ const td = new Todo(
   }
 )
 
+// next steps: create an object (array) for holding all projects
+// unless no projects exist, there should alway be a project selected by default
+// if a selected project is deleted, another is automatically selected
+// a new todo is attached to the selected project
+// if no project exists, it should not be possible to create a todo
+// alternatively, a default project that can not be deleted holds all non-project todos
+
 defaultProject.addTodo(td)
 console.log(defaultProject.projectTodos)
 defaultProject.removeTodo(td)
@@ -44,40 +51,44 @@ const createTodo = function(args) {
 
 const buttons = [projectModalBtn, todoModalBtn]
 
+const createObject = function(modal, button) {
+  // have the same class of 'input' on all input fields
+  const inputFields = modal.querySelectorAll('.input')
+  const constructorArgs = {}
+  // use name property of inputs to create the key-words: {'title': 'some title', 'description': 'blabla'}
+  inputFields.forEach(inputField => {
+    constructorArgs[inputField.name] = inputField.value
+  })
+  if (button.id === 'create-project') {
+    if (constructorArgs['title'].trim()) {
+      createProject(constructorArgs)
+      ui.closeModal(modal)
+    }
+  }
+  else if (button.id === 'create-todo') {
+    if (constructorArgs['title'].trim() && constructorArgs['priority'] && constructorArgs['dueDate']) {
+      createTodo(constructorArgs)
+      ui.closeModal(modal)
+    }
+  }
+}
+
 buttons.forEach(button => {
-  button.onclick = function() {
+  button.addEventListener('click', function() {
     const modal = button.nextElementSibling
     ui.openModal(modal)
 
     const submitButton = modal.querySelector('input[type="submit"]')
-    submitButton.onclick = function(event) {
+    submitButton.addEventListener('click', function(event) {
       event.preventDefault();
-      // have the same class of 'input' on all input fields
-      const inputFields = modal.querySelectorAll('.input')
-      const constructorArgs = {}
-      // use name property of inputs to create the key-words: {'title': 'some title', 'description': 'blabla'}
-      inputFields.forEach(inputField => {
-        constructorArgs[inputField.name] = inputField.value
-      })
-      if (submitButton.id === 'create-project') {
-        if (constructorArgs['title'].trim()) {
-          createProject(constructorArgs)
-          ui.closeModal(modal)
-        }
-      }
-      else if (submitButton.id === 'create-todo') {
-        if (constructorArgs['title'].trim() && constructorArgs['priority'] && constructorArgs['dueDate']) {
-          createTodo(constructorArgs)
-          ui.closeModal(modal)
-        }
-      }
-    }
+      createObject(modal, submitButton)
+    })
 
     const closeSpan = modal.querySelector('.close')
-    window.onclick = function(event) {
+    window.addEventListener('click', function(event) {
       if (event.target == modal || event.target == closeSpan) {
         ui.closeModal(modal, false)
       }
-    }
-  }
+    })
+  })
 })
