0fd52662272c8f1257cea2caa30d7c642a0301f9
lbackman <lucasbackman@gmail.com>
2024-06-05 10:21:45 +0300

    Remove 'selectable' class from todo template

--- src/todo_template.js
+++ src/todo_template.js
@@ -1,16 +1,16 @@
 export function todoTemplate(todo) {
   const todoDiv = document.createElement('div')
-  todoDiv.classList.add('todo','selectable', 'deletable', 'editable')
+  todoDiv.classList.add('todo', 'deletable', 'editable')
   todoDiv.dataset.id = todo.id
   todoDiv.tabIndex = '0'
 
-  const contents = `<h2 class="title selectable">${todo.title}</h2>
-                    <p class="description selectable">${todo.description}</p>
-                    <p class="description selectable">Due date: ${todo.dueDate}</p>
-                    <p class="description selectable">Priority: ${todo.priority}</p>
-                    <p class="description selectable">Status: ${todo.currentStatus}</p>
+  const contents = `<h2 class="title">${todo.title}</h2>
+                    <p class="description">${todo.description}</p>
+                    <p class="due-date">Due date: ${todo.dueDate}</p>
+                    <p class="priority">Priority: ${todo.priority}</p>
+                    <p class="current-status">Status: ${todo.currentStatus}</p>
                     <button class="delete">Delete</button>
-                    <button class="edit selectable">Edit</button>`
+                    <button class="edit">Edit</button>`
 
   todoDiv.insertAdjacentHTML('afterbegin', contents)
 
