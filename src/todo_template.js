0d45667647e1d4a3ef6d90e74ed956acace26949
lbackman <lucasbackman@gmail.com>
2024-06-12 10:24:47 +0300

    Add checkbox to todo template

--- src/todo_template.js
+++ src/todo_template.js
@@ -3,16 +3,19 @@ export function todoTemplate(todo) {
   todoDiv.classList.add('todo', 'deletable', 'editable')
   todoDiv.dataset.id = todo.id
   todoDiv.tabIndex = '0'
-
-  const contents = `<h2 class="title">${todo.title}</h2>
-                    <p class="description">${todo.description}</p>
-                    <p class="due-date">Due date: ${todo.dueDate}</p>
-                    <p class="priority">Priority: ${todo.priority}</p>
-                    <p class="current-status">Status: ${todo.currentStatus}</p>
-                    <button class="delete">Delete</button>
-                    <button class="edit">Edit</button>`
-
+  const checkbox = `<input type="checkbox" class="check">`
+  const contents = `<div>
+                      <h2 class="title">${todo.title}</h2>
+                      <p class="description">${todo.description}</p>
+                      <p class="due-date">Due date: ${todo.dueDate}</p>
+                      <p class="priority">Priority: ${todo.priority}</p>
+                      <p class="current-status">Status: ${todo.currentStatus}</p>
+                      <button class="delete">Delete</button>
+                      <button class="edit">Edit</button>
+                    </div>`
+  
   todoDiv.insertAdjacentHTML('afterbegin', contents)
+  todoDiv.insertAdjacentHTML('afterbegin', checkbox)
 
   return todoDiv
 }
